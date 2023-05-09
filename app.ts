abb0673e8020a2be613feb7a2372614cee39ac5d
Shahzaib786 <125633854+shahzaibee@users.noreply.github.com>
2023-05-10 01:17:24 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+
+async function startCountdown() {
+  const { duration } = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'duration',
+      message: 'Enter the duration of the countdown in seconds:',
+      validate: (value) => {
+        const parsedValue = parseInt(value);
+        if (isNaN(parsedValue)) {
+          return 'Please enter a valid number';
+        }
+        return true;
+      },
+    },
+  ]);
+
+  console.log(chalk.blue(`Starting countdown for ${duration} seconds...\n`));
+
+  let remainingTime = parseInt(duration);
+
+  const intervalId = setInterval(() => {
+    remainingTime--;
+
+    if (remainingTime <= 0) {
+      clearInterval(intervalId);
+      console.log(chalk.green('\nCountdown completed!\n'));
+      return;
+    }
+
+    console.log(chalk.yellow(`Remaining time: ${remainingTime} seconds`));
+  }, 1000);
+}
+
+startCountdown();
+
